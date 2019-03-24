18d745910759e9031b15bef207b0add8f12bbfd5
OoGod <1617605689@qq.com>
2019-03-24 21:11:10 +0800

    update file

--- douban.py
+++ douban.py
@@ -7,7 +7,8 @@ class douban(unittest.TestCase):
     
     def setUp(self):
         self.driver = webdriver.PhantomJS()
-        
+        self.x = 0 
+
     def testDouban(self):
         self.driver.get("https://movie.douban.com/subject/4739952/reviews")
         
@@ -15,11 +16,15 @@ class douban(unittest.TestCase):
             soup = bs(self.driver.page_source,"lxml")
             names = soup.find_all("a",{"class":"name"})
             times = soup.find_all("span",{"class":"main-meta"})
+            #reviews = soup.find_all("div",{"class":"review-content"})
             for name,time in zip(names,times):
                 print("昵称: ",name.get_text().strip(),"\t时间: ",time.get_text().strip())
+                #print("评论：",review.get_text().strip())
 
-            if self.driver.page_source.find("btn-unfold") != -1:
+            self.driver.
+            if self.driver.page_source.find("btn-unfold") != -1 or self.x == 3:
                 break
+            self.x += 1
             self.driver.find_element_by_class_name("next").click()
     def tearDown(self):
         self.driver.quit()    
